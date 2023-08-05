84dcfaacf63d608b9d2a17120f3f894f37c0d1d9
kunal raj shah <rajkunal7279@gmail.com>
2023-08-05 20:32:48 +0530

    store input data using ref HOOK

--- src/Component/Form.js
+++ src/Component/Form.js
@@ -1,38 +1,44 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import InvalidDiv from "./InvalidDiv";
 
-
 const Form = (props) => {
-  const [inputText, setInputText] = useState("");
-  const [inputAge, setInputAge] = useState("");
+  // Using ref hook
+  const nameInputRef = useRef();
+  const AgeInputRef = useRef();
+
+  // const [inputText, setInputText] = useState("");
+  // const [inputAge, setInputAge] = useState("");
   const [showInvalidDiv, setShowInvalidDiv] = useState();
 
-  const textHandler = (event) => {
-    setInputText(event.target.value);
-  };
+  // const textHandler = (event) => {
+  //   setInputText(event.target.value);
+  // };
 
-  const ageHandler = (event) => {
-    setInputAge(event.target.value);
-  };
-
-  const obj = {
-    name: inputText,
-    age: inputAge,
-    id: Math.random().toString(),
-  };
+  // const ageHandler = (event) => {
+  //   setInputAge(event.target.value);
+  // };
 
   const getDataHandler = (event) => {
     event.preventDefault();
+    const enteredName = nameInputRef.current.value;
+    const enteredAge = AgeInputRef.current.value;
+
+    const obj = {
+      name: enteredName,
+      age: enteredAge,
+      id: Math.random().toString(),
+    };
+
     // Form Validation
-    if (inputText.trim().length === 0 || inputAge.trim().length === 0) {
+    if (enteredName.trim().length === 0 || enteredAge.trim().length === 0) {
       setShowInvalidDiv({
         title: "Invalid Input",
         message: "Please enter a valid name and age (non-empty values)",
       });
       return;
     }
-    
-    if (+inputAge < 1) {
+
+    if (+enteredAge < 1) {
       //+ is used to convert string into number
       setShowInvalidDiv({
         title: "Invalid Age",
@@ -40,15 +46,16 @@ const Form = (props) => {
       });
       return;
     }
-
+    // Sending Data to another component
     props.get(obj);
-    setInputText("");
-    setInputAge("");
+    // Clear input data
+    nameInputRef.current.value = "";
+    AgeInputRef.current.value = "";
   };
 
   return (
     <>
-    {/* If showInvalidDiv is truthy (not false, null, undefined, 0, or an empty string), the expression will evaluate to the value on the right-hand side (...). Otherwise, if showInvalidDiv is falsy, the expression will be false, and nothing will be rendered. */}
+      {/* If showInvalidDiv is truthy (not false, null, undefined, 0, or an empty string), the expression will evaluate to the value on the right-hand side (...). Otherwise, if showInvalidDiv is falsy, the expression will be false, and nothing will be rendered. */}
       {showInvalidDiv && (
         <InvalidDiv
           title={showInvalidDiv.title}
@@ -60,11 +67,17 @@ const Form = (props) => {
         <form>
           <label>User Name</label>
           <br />
-          <input type="text" value={inputText} onChange={textHandler}></input>
+          <input
+            type="text"
+            /*value={inputText} onChange={textHandler}*/ ref={nameInputRef}
+          ></input>
           <br />
           <label>Age(In Years)</label>
           <br />
-          <input type="number" value={inputAge} onChange={ageHandler}></input>
+          <input
+            type="number"
+            /*value={inputAge} onChange={ageHandler}*/ ref={AgeInputRef}
+          ></input>
           <br />
           <button onClick={getDataHandler}>Add Users</button>
         </form>
